6ef084d66a89c7f46c2452266f8243a82909dd80
José Castro <a.j.castro.b@gmail.com>
2026-06-04 01:52:05 -0500

    refactor: webcomponents

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { HtmlHeaderTvShows } from "./modules/HtmlHeader"
+import { HtmlHeaderTvShows } from "./modules/HtmlHeader";
 import { SeasonTvShows } from "./modules/HtmlTvShow";
 
 customElements.define("header-tv-show", HtmlHeaderTvShows);
 customElements.define("season-tv", SeasonTvShows);
-
